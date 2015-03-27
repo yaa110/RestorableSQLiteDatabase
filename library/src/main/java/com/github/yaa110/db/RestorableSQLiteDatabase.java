025baaa181f2624ac666b1dbf8283b20dcf9fcc2
Navid <yaa110@gmail.com>
2015-03-27 20:41:29 +0430

    Create the methods of hash table

--- library/src/main/java/com/github/yaa110/db/RestorableSQLiteDatabase.java
+++ library/src/main/java/com/github/yaa110/db/RestorableSQLiteDatabase.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import java.util.Hashtable;
+import java.util.Set;
 
 /**
  * A wrapper to replicate the `SQLiteDatabase` class to manage a SQLite database with restoring capability.
@@ -19,13 +20,12 @@ public class RestorableSQLiteDatabase {
     private static final String TAG = "SQLiteDatabase";
 
     /**
-     * The hash table to map the tag of a restoring statement to its query
+     * The hash table to map a tag to its restoring query
      */
     public Hashtable<String, String> mTagQueryTable;
 
     public static RestorableSQLiteDatabase getInstance(SQLiteDatabase mSQLiteDatabase){
-        if(mInstance == null)
-        {
+        if(mInstance == null) {
             mInstance = new RestorableSQLiteDatabase(mSQLiteDatabase);
         }
         return mInstance;
@@ -33,17 +33,53 @@ public class RestorableSQLiteDatabase {
 
     /**
      * Private constructor of singleton pattern
-     * @param mSQLiteDatabase the instance of SQLiteDatabase to be wrapped
+     * @param mSQLiteDatabase the instance of the SQLiteDatabase to be wrapped
      */
     private RestorableSQLiteDatabase(SQLiteDatabase mSQLiteDatabase) {
+        mTagQueryTable = new Hashtable<>();
         this.mSQLiteDatabase = mSQLiteDatabase;
     }
 
     /**
-     * @return the wrapped SQLiteDatabase instance
+     * Provides the instance of wrapped SQLiteDatabase
+     * @return the instance of wrapped SQLiteDatabase
      */
     public SQLiteDatabase getSQLiteDatabase() {
         return mSQLiteDatabase;
+    }
+
+    /**
+     * Checks if the hash table contains the tag
+     * @param tag Possible tag of restoring query
+     * @return true if the hash table contains the tag; false otherwise
+     */
+    public boolean containsTag(String tag) {
+        return mTagQueryTable.containsKey(tag);
+    }
+
+    /**
+     * Provides a Set view of the tags contained in the hash table
+     * @return a Set view of the tags contained in the hash table
+     */
+    public Set<String> tagSet() {
+        return mTagQueryTable.keySet();
+    }
+
+    /**
+     * Provides the query to which the tag is mapped
+     * @param tag Possible tag of restoring query
+     * @return the query to which the tag is mapped, or null if the hash table contains no mapping for the tag
+     */
+    public String getQuery(String tag) {
+        return mTagQueryTable.get(tag);
+    }
+
+    /**
+     * Returns the hash table
+     * @return the hash table
+     */
+    public Hashtable<String, String> getTagQueryTable() {
+        return mTagQueryTable;
     }
 
     /**
