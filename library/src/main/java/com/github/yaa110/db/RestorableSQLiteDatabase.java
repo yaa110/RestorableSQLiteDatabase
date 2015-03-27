cd6632949cecd8cb9930e424fc942430c177dea8
Navid <yaa110@gmail.com>
2015-03-27 18:39:15 +0430

    initial commit

---
+++ library/src/main/java/com/github/yaa110/db/RestorableSQLiteDatabase.java
@@ -0,0 +1,12 @@
+package com.github.yaa110.db;
+
+
+/**
+ * A wrapper to replicate the `SQLiteDatabase` class to manage a SQLite database with restoring capability.
+ * This wrapper makes it possible to undo changes made after execution of SQL commands.
+ */
+public class RestorableSQLiteDatabase {
+
+
+
+}
