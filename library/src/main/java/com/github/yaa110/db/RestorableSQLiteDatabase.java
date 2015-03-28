9018b8bc80774f48db0694ec83398a53f70da94b
Navid <yaa110@gmail.com>
2015-03-28 21:43:30 +0430

    Add JSqlParser maven repository from github

--- library/src/main/java/com/github/yaa110/db/RestorableSQLiteDatabase.java
+++ library/src/main/java/com/github/yaa110/db/RestorableSQLiteDatabase.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.CancellationSignal;
 import android.util.Log;
 
 import java.util.ArrayList;
